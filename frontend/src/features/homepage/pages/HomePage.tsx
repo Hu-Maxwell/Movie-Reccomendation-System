a045ad4cfa4e7084fb8346c9ac7583f8f18e3e9b
Vchen7629 <chenvincent7629@gmail.com>
2025-09-29 01:29:44 -0700

    chore: triggering frontend update

--- frontend/src/features/homepage/pages/HomePage.tsx
+++ frontend/src/features/homepage/pages/HomePage.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count}!
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
